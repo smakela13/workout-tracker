32e094c6ac6f97f962f960f53a8d6cb2caccb886
SMakela <73409799+smakela13@users.noreply.github.com>
2021-06-08 10:51:43 -0400

    Completed routes in api.js - updated gitignore to use eslint and prettier - polished public/api.js and routes/api.js with eslint/prettier

--- routes/api.js
+++ routes/api.js
@@ -1,35 +1,58 @@
-const router = require("express").Router();
-const Workout = require("../models/workout.js");
+/* eslint-disable new-cap */
+const router = require('express').Router();
+const Workout = require('../models/workout.js');
+const path = require('path');
 
-router.post("/api/workout", ({ body }, res) => {
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+router.get('/stats', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/stats.html'));
+});
+
+router.get('/exercise', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/exercise.html'));
+});
+
+router.get('/api/workouts', (req, res) => {
+  Workout.find()
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+  Workout.find()
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+});
+
+router.post('/api/workouts', ({body}, res) => {
   Workout.create(body)
-    .then((workoutData) => {
-      res.json(workoutData);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
 });
 
-router.post("/api/workout", ({ body }, res) => {
-  Workout.insertMany(body)
-    .then((workoutData) => {
-      res.json(workoutData);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/workout", (req, res) => {
-  Workout.find({})
-    .sort({ date: -1 })
-    .then((workoutData) => {
-      res.json(workoutData);
-    })
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+router.put('/api/workouts/:id', ({body, params}, res) => {
+  Workout.findByIdAndUpdate(params.id, {$push: {exercises: body}})
+      .then((workoutData) => {
+        res.json(workoutData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
 });
 
 module.exports = router;
